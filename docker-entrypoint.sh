db60a64970c76b4b17fe27cc4edffe72309d0eca
john ross <johnhoundji@gmail.com>
2026-03-20 19:25:53 +0100

    ajout cache commandes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,7 @@
 php artisan migrate:fresh --force
 php artisan db:seed --force
 php artisan storage:link
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 apache2-foreground
