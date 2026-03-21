024a795c212ed2288e0eea9b229432b3589d7e45
john ross <johnhoundji@gmail.com>
2026-03-21 09:31:02 +0100

    CSS via fichier statique public

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
     <title>@yield('title', 'ArtisanConnect')</title>
     
     <script src="https://cdn.tailwindcss.com"></script>
