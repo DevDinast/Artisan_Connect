a57d0a38c18fc12b1867634de02185c3ea335051
john ross <johnhoundji@gmail.com>
2026-03-21 09:21:51 +0100

    suppression vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'ArtisanConnect')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         .nav-panier {
